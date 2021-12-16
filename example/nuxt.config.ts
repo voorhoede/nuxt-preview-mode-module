04d4568e33543ea3ac04d75cbbec04eb50e96d86
Frank Warnaar <frankwarnaar@gmail.com>
2021-12-16 13:57:11 +0100

    feat(example): typescript setup

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -1,11 +1,16 @@
+import { NuxtConfig } from '@nuxt/types';
+
 import { join } from 'path';
 
-export default {
+const config: NuxtConfig = {
   target: 'static',
   buildModules: [
+    '@nuxt/typescript-build',
     join(__dirname, '../lib/module.js'),
   ],
   previewMode: {
     previewSecret: 'secret',
   },
 };
+
+export default config;
--- example/pages/index.vue
+++ example/pages/index.vue
@@ -4,3 +4,17 @@
     <p>Preview enabled: {{ $nuxt.isPreview }}</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  mounted () {
+    this.$previewMode.enter();
+
+    setTimeout(() => {
+      this.$previewMode.exit();
+    }, 5000);
+  },
+});
+</script>
