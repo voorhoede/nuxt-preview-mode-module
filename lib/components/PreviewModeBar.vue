6a074d8bb8b90cfd21e19708cb850eff1138c957
Frank Warnaar <frankwarnaar@gmail.com>
2021-12-20 11:17:46 +0100

    feat: export components

---
+++ lib/components/PreviewModeBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <client-only v-if="$nuxt.isPreview">
+    <slot>
+      <div>
+        <p>You're currently in preview mode</p>
+        <preview-mode-exit-button />
+      </div>
+    </slot>
+  </client-only>
+</template>
+
+<script>
+import PreviewModeExitButton from './PreviewModeExitButton.vue';
+
+export default {
+  components: {
+    PreviewModeExitButton,
+  },
+};
+</script>
---
+++ lib/components/PreviewModeExitButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button type="button" @click="$previewMode.exit">
+    <slot>Exit preview</slot>
+  </button>
+</template>
